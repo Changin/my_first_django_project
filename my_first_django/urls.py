18f0b48109a6363bc373c380c0c6475e6acaa760
ChangIn <qorckddls010@naver.com>
2018-01-20 17:00:45 +0900

    Update templates

--- my_first_django/urls.py
+++ my_first_django/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('first_app/', include('first_app.urls')),
+    path('first_app/', include('first_app.urls'), name='first_app'),
 ]
